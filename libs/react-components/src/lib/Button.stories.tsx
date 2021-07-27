5c937fe412f0e0cbdd9b7905ddf2e8b768a7f9df
Peter Holló <hello@songsforthe.dev>
2021-07-27 12:39:22 +0200

    chore: set up storybook

---
+++ libs/react-components/src/lib/Button.stories.tsx
@@ -0,0 +1,12 @@
+import { Story, Meta } from "@storybook/react";
+import { Button, ButtonProps } from "./Button";
+
+export default {
+  component: Button,
+  title: "Button",
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
