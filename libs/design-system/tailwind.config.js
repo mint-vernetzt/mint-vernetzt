f13d3d8bf03e64cd90758f123b37f9570843d80f
Peter Holló <hello@songsforthe.dev>
2021-07-28 17:34:34 +0200

    chore(design-system): add current colors to theme

--- libs/design-system/tailwind.config.js
+++ libs/design-system/tailwind.config.js
@@ -1,8 +1,40 @@
+const customColors = {
+  beige: "#f4e8e0",
+  blue: "#164194",
+  lilac: "#c8acd3",
+  yellow: "#fdc533",
+  green: "#42b38c",
+  salmon: "#f6afad",
+};
+
+const extendedCustomColors = {
+  ...customColors,
+  success: customColors.green,
+  warning: customColors.yellow,
+  danger: customColors.salmon,
+};
+
+let colorSafelist = [];
+Object.keys(extendedCustomColors).forEach((color) => {
+  colorSafelist = colorSafelist.concat([
+    `bg-${color}`,
+    `text-${color}`,
+    `border-${color}`,
+  ]);
+});
+
 module.exports = {
-  purge: ["../**/*.ts", "../**/*.tsx"],
+  purge: {
+    content: ["../**/*.ts", "../**/*.tsx"],
+    safelist: [...colorSafelist],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...extendedCustomColors,
+      },
+    },
   },
   variants: {
     extend: {},
