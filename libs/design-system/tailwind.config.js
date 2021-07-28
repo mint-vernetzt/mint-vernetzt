27d3242848281322b8b099dfd006eb090450b26a
Peter Holló <hello@songsforthe.dev>
2021-07-28 13:53:53 +0200

    fix(design-system): adjust paths for purge css
    before all used classes were purged

--- libs/design-system/tailwind.config.js
+++ libs/design-system/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["../react-components/**/*.stories.tsx"],
+  purge: ["../**/*.ts", "../**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
