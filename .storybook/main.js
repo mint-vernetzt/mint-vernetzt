3ce5f327d42bc0e6cbaa8b111f719bf823089ce4
Peter Holló <hello@songsforthe.dev>
2021-08-10 09:20:00 +0200

    chore: add dark mode plugin to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: [],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-a11y"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "storybook-dark-mode",
+  ],
 };
