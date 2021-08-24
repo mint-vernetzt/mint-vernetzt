2a6ee86c5403fb5dd3c73f4c69e20334761fad55
Peter Holló <hello@songsforthe.dev>
2021-08-24 15:34:07 +0200

    test(react-components): add render and click test to button

--- libs/react-components/src/lib/Button.spec.tsx
+++ libs/react-components/src/lib/Button.spec.tsx
@@ -1,10 +1,23 @@
-import { render } from "@testing-library/react";
-
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
 import Button from "./Button";
 
-describe("Button", () => {
-  it("should render successfully", () => {
-    const { baseElement } = render(<Button />);
-    expect(baseElement).toBeTruthy();
-  });
+test("render children", () => {
+  const text = "Button";
+
+  const { getByText } = render(<Button>{text}</Button>);
+
+  expect(getByText(text)).toBeTruthy();
+});
+
+test("click handler", () => {
+  const handler = jest.fn();
+  const text = "Button";
+
+  render(<Button onClick={handler}>{text}</Button>);
+
+  const button = screen.getByText(text);
+  user.click(button);
+
+  expect(handler).toHaveBeenCalledTimes(1);
 });
