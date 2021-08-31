f30cac1554c5ae18b1ba2b1627943452604bf8ac
Peter Holló <hello@songsforthe.dev>
2021-08-31 14:41:03 +0200

    feat(website): add styles from design system

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-import { ReactComponent as Logo } from "./logo.svg";
-import star from "./star.svg";
+import "../../../../libs/design-system/src/styles.css";
 
 export function Index() {
   return <h1>Welcome to website!</h1>;
