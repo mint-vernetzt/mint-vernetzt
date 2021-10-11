7f392318d889ca9afad3ffebbefce23a87cbdbf6
Peter Holló <hello@songsforthe.dev>
2021-10-11 17:35:41 +0200

    test utility functions

--- libs/react-components/src/lib/NewsFeed/utils.tsx
+++ libs/react-components/src/lib/NewsFeed/utils.tsx
@@ -1,6 +1,7 @@
 import faker from "faker";
+import { TagProps } from "./NewsFeedItem";
 
-export function formatDate(date: Date) {
+export function formatDate(date: Date): string {
   const formatted = date.toLocaleDateString("de-DE", {
     day: "2-digit",
     month: "long",
@@ -9,7 +10,7 @@ export function formatDate(date: Date) {
   return formatted;
 }
 
-export function getTag() {
+export function getTag(): TagProps {
   const title = faker.lorem.word();
   return { title };
 }
