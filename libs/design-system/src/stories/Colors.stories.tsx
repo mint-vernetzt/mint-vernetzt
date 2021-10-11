804a27e0375db497c5f757ac2f0d9d3a37d04610
Peter Holló <hello@songsforthe.dev>
2021-10-11 12:35:19 +0200

    update colors

--- libs/design-system/src/stories/Colors.stories.tsx
+++ libs/design-system/src/stories/Colors.stories.tsx
@@ -8,82 +8,129 @@ export const Colors: Story = () => {
         <div className="flex items-center">
           <div className="flex-none bg-primary w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
           <div className="flex-1">
-            <p className="font-bold text-sm">primary</p>
+            <p className="font-bold text-sm">primary/primary-500</p>
             <p className="text-sm">{colors.primary}</p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="flex-none bg-primary-400 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
+          <div className="flex-1">
+            <p className="font-bold text-sm">primary-400</p>
+            <p className="text-sm">{colors["primary-400"]}</p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="flex-none bg-primary-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
+          <div className="flex-1">
+            <p className="font-bold text-sm">primary-300</p>
+            <p className="text-sm">{colors["primary-300"]}</p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="flex-none bg-primary-200 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
+          <div className="flex-1">
+            <p className="font-bold text-sm">primary-200</p>
+            <p className="text-sm">{colors["primary-200"]}</p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="flex-none bg-primary-100 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
+          <div className="flex-1">
+            <p className="font-bold text-sm">primary-100</p>
+            <p className="text-sm">{colors["primary-100"]}</p>
           </div>
         </div>
         <div className="flex items-center">
           <div className="flex-none bg-secondary w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
           <div className="flex-1">
-            <p className="font-bold text-sm">secondary</p>
+            <p className="font-bold text-sm">secondary/secondary-500</p>
             <p className="text-sm">{colors.secondary}</p>
           </div>
         </div>
         <div className="flex items-center">
-          <div className="flex-none bg-fill w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
+          <div className="flex-none bg-secondary-400 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
           <div className="flex-1">
-            <p className="font-bold text-sm">fill</p>
-            <p className="text-sm">{colors.fill}</p>
+            <p className="font-bold text-sm">secondary-400</p>
+            <p className="text-sm">{colors["secondary-400"]}</p>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-5 gap-2 w-full">
+        <div className="flex items-center">
+          <div className="flex-none bg-secondary-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
+          <div className="flex-1">
+            <p className="font-bold text-sm">secondary-300</p>
+            <p className="text-sm">{colors["secondary-300"]}</p>
+          </div>
+        </div>
+        <div></div>
         <div></div>
         <div className="flex items-center">
-          <div className="flex-none bg-secondary-light w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
+          <div className="flex-none bg-tertiary w-1 h-1 p-4 mr-1 rounded-md border border-transparent border-neutral-500"></div>
           <div className="flex-1">
-            <p className="font-bold text-sm">secondary-light</p>
-            <p className="text-sm">{colors["secondary-light"]}</p>
+            <p className="font-bold text-sm">tertiary/tertiary-500</p>
+            <p className="text-sm">{colors.tertiary}</p>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-5 gap-2 w-full">
+        <div className="flex items-center">
+          <div className="flex-none bg-tertiary-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent border-neutral-500"></div>
+          <div className="flex-1">
+            <p className="font-bold text-sm">tertiary-300</p>
+            <p className="text-sm">{colors["tertiary-300"]}</p>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
         <div className="flex items-center">
           <div className="flex-none bg-success w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
           <div className="flex-1">
-            <p className="font-bold text-sm">success</p>
+            <p className="font-bold text-sm">success/success-500</p>
             <p className="text-sm">{colors.success}</p>
           </div>
         </div>
         <div className="flex items-center">
+          <div className="flex-none bg-success-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
+          <div className="flex-1">
+            <p className="font-bold text-sm">success-300</p>
+            <p className="text-sm">{colors["success-300"]}</p>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div className="flex items-center">
           <div className="flex-none bg-warning w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
           <div className="flex-1">
-            <p className="font-bold text-sm">warning</p>
+            <p className="font-bold text-sm">warning/warning-500</p>
             <p className="text-sm">{colors.warning}</p>
           </div>
         </div>
         <div className="flex items-center">
+          <div className="flex-none bg-warning-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
+          <div className="flex-1">
+            <p className="font-bold text-sm">warning-300</p>
+            <p className="text-sm">{colors["warning-300"]}</p>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div className="flex items-center">
           <div className="flex-none bg-danger w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
           <div className="flex-1">
-            <p className="font-bold text-sm">danger</p>
+            <p className="font-bold text-sm">danger/danger-500</p>
             <p className="text-sm">{colors.danger}</p>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-5 gap-2 w-full">
         <div className="flex items-center">
-          <div className="flex-none bg-success-light w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
+          <div className="flex-none bg-danger-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
           <div className="flex-1">
-            <p className="font-bold text-sm">success-light</p>
-            <p className="text-sm">{colors["success-light"]}</p>
+            <p className="font-bold text-sm">danger-300</p>
+            <p className="text-sm">{colors["danger-300"]}</p>
           </div>
         </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-warning-light w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">warning-light</p>
-            <p className="text-sm">{colors["warning-light"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-danger-light w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">danger-light</p>
-            <p className="text-sm">{colors["danger-light"]}</p>
-          </div>
-        </div>
-      </div>
-      <div className="grid grid-cols-5 gap-2 w-full">
+        <div></div>
+        <div></div>
+        <div></div>
         <div className="flex items-center">
           <div className="flex-none bg-neutral-900 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
           <div className="flex-1">
@@ -119,8 +166,6 @@ export const Colors: Story = () => {
             <p className="text-sm">{colors["neutral-500"]}</p>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-5 gap-2 w-full">
         <div className="flex items-center">
           <div className="flex-none bg-neutral-400 w-1 h-1 p-4 mr-1 rounded-md border border-neutral-500"></div>
           <div className="flex-1">
