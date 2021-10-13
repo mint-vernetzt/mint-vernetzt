f0e950f6f042d6ac44b086fbace01f2e06009925
Peter Holló <hello@songsforthe.dev>
2021-10-13 09:31:35 +0200

    add missing size 1.5rem Resolve https://github.com/mint-vernetzt/mint-vernetzt/pull/50#discussion_r727772000

--- libs/design-system/src/theme.js
+++ libs/design-system/src/theme.js
@@ -59,10 +59,11 @@ const fontSizes = {
   lg: "1.125rem",
   xl: "1.1875rem",
   "2xl": "1.25rem",
-  "3xl": "2rem",
-  "4xl": "2.5rem",
-  "5xl": "4rem",
-  "6xl": "6rem",
+  "3xl": "1.5rem",
+  "4xl": "2rem",
+  "5xl": "2.5rem",
+  "6xl": "4rem",
+  "7xl": "6rem",
 };
 
 module.exports = {
