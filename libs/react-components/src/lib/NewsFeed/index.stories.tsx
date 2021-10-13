eb08ff333f314e7c6a71f01d1f06a85714a5225e
Peter Holló <hello@songsforthe.dev>
2021-10-13 10:22:58 +0200

    merge stories

--- libs/react-components/src/lib/NewsFeed/index.stories.tsx
+++ libs/react-components/src/lib/NewsFeed/index.stories.tsx
@@ -1,20 +1,32 @@
 import { Meta, Story } from "@storybook/react";
 import faker from "faker";
+import { NewsFeed, NewsFeedProps } from "./NewsFeed";
 import { NewsFeedItem, NewsFeedItemProps } from "./NewsFeedItem";
-import { getTag } from "./utils";
+import { getNewsFeedItemProps, getTag } from "./utils";
 
 export default {
   component: NewsFeedItem,
   title: "Components/NewsFeed",
 } as Meta;
+const Template: Story<NewsFeedProps> = (args) => <NewsFeed {...args} />;
 
 export const Item: Story<NewsFeedItemProps> = (args) => (
   <NewsFeedItem {...args} />
 );
-Item.storyName = "item";
 Item.args = {
   headline: faker.lorem.words(),
   body: faker.lorem.paragraph(),
   date: faker.date.future(),
   tagsProps: [getTag(), getTag(), getTag()],
 };
+Item.storyName = "item";
+
+export const Feed = Template.bind({});
+Feed.args = {
+  headline: faker.lorem.words(),
+  body: faker.lorem.paragraph(),
+  newsFeedItemsProps: new Array(7).fill(null).map(() => {
+    return getNewsFeedItemProps();
+  }),
+};
+Feed.storyName = "feed";
