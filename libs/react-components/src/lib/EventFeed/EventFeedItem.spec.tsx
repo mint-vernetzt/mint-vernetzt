1ab9f40c1834259695ecb28120fd005fc722afcf
Peter Holló <hello@songsforthe.dev>
2021-10-13 11:26:53 +0200

    implement elements of event feed item

--- libs/react-components/src/lib/EventFeed/EventFeedItem.spec.tsx
+++ libs/react-components/src/lib/EventFeed/EventFeedItem.spec.tsx
@@ -1,10 +1,23 @@
-import { render } from "@testing-library/react";
-
+import { render, screen } from "@testing-library/react";
+import faker from "faker";
 import EventFeedItem from "./EventFeedItem";
+import { formatDate } from "./utils";
 
-describe("EventFeedItem", () => {
-  it("should render successfully", () => {
-    const { baseElement } = render(<EventFeedItem />);
-    expect(baseElement).toBeTruthy();
-  });
+test("render content elements", () => {
+  const props = {
+    headline: faker.lorem.words(),
+    body: faker.lorem.paragraphs(),
+    date: faker.date.future(),
+  };
+
+  render(<EventFeedItem {...props} />);
+
+  const headlineDom = screen.getByRole("heading");
+  expect(headlineDom.textContent).toBe(props.headline);
+
+  const bodyDom = screen.getByTestId("body");
+  expect(bodyDom.textContent).toBe(props.body);
+
+  const dateDom = screen.getByTestId("date");
+  expect(dateDom.textContent).toBe(formatDate(props.date));
 });
--- libs/react-components/src/lib/EventFeed/EventFeedItem.tsx
+++ libs/react-components/src/lib/EventFeed/EventFeedItem.tsx
@@ -1,10 +1,25 @@
-/* eslint-disable-next-line */
-export interface EventFeedItemProps {}
+import React from "react";
+import { formatDate } from "./utils";
 
-export function EventFeedItem(props: EventFeedItemProps) {
+export interface EventFeedItemProps {
+  headline: string;
+  body: string;
+  date: Date;
+}
+
+export function EventFeedItem({ headline, body, date }: EventFeedItemProps) {
+  const [formattedDate, setFormattedDate] = React.useState<string>();
+
+  React.useEffect(() => {
+    setFormattedDate(formatDate(date));
+  }, [date]);
   return (
-    <div>
-      <h1>Welcome to EventFeedItem!</h1>
+    <div className="relative">
+      <time data-testid="date" dateTime={date.toISOString()}>
+        {formattedDate}
+      </time>
+      <h3>{headline}</h3>
+      <p data-testid="body">{body}</p>
     </div>
   );
 }
