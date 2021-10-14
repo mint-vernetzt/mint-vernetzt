57314b262bff090fe6a209b6efc335a73ff8b065
Peter Holló <hello@songsforthe.dev>
2021-10-14 10:36:11 +0200

    fix: wrong breakpoints in font size switch

--- libs/react-components/src/lib/NewsFeed/NewsFeed.tsx
+++ libs/react-components/src/lib/NewsFeed/NewsFeed.tsx
@@ -16,14 +16,14 @@ export function NewsFeed({
   return (
     <section>
       {/* TODO: remove uppercase in tailwind config and override in component headline */}
-      <h3 className="mb-1 text-primary normal-case text-5xl leading-none md:text-6xl sm:mb-6">
+      <h3 className="mb-1 text-primary normal-case text-5xl leading-none lg:text-6xl sm:mb-6">
         <a className="cursor-pointer hover:underline" href={linkToOverview}>
           {headline}
         </a>
       </h3>
       <p
         data-testid="body"
-        className="text-neutral-800 text-2xl leading-tight"
+        className="text-neutral-800 text-base lg:text-2xl font-bold lg:font-normal leading-tight"
         style={{ marginBottom: "7px" }}
       >
         {body}
