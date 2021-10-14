27fd1903013502838b56278dbc8d49810f6a8af8
Peter Holló <hello@songsforthe.dev>
2021-10-14 10:09:21 +0200

    add todo related to override default shadows Resolve https://github.com/mint-vernetzt/mint-vernetzt/pull/53#discussion_r728110130

--- libs/react-components/src/lib/EventFeed/EventFeed.tsx
+++ libs/react-components/src/lib/EventFeed/EventFeed.tsx
@@ -12,21 +12,24 @@ export function EventFeed({
   eventFeedItemsProps = [],
 }: EventFeedProps) {
   return (
-    <div className="m-4 px-8 py-6 rounded-lg bg-neutral-200 shadow-xl">
-      <h3 className="mb-px text-3xl leading-none text-primary font-bold md:text-4xl">
-        <a href={linkToOverview} className="cursor-pointer hover:underline">
-          {headline}
-        </a>
-      </h3>
-      {eventFeedItemsProps.map((eventFeedItemProps, index) => {
-        return (
-          <EventFeedItem
-            key={`event-feed-item-${index}`}
-            {...eventFeedItemProps}
-          />
-        );
-      })}
-    </div>
+    <>
+      {/* TODO: replace default shadows with shadows from design in tailwind config */}
+      <div className="m-4 px-8 py-6 rounded-lg bg-neutral-200 shadow-xl">
+        <h3 className="mb-px text-3xl leading-none text-primary font-bold md:text-4xl">
+          <a href={linkToOverview} className="cursor-pointer hover:underline">
+            {headline}
+          </a>
+        </h3>
+        {eventFeedItemsProps.map((eventFeedItemProps, index) => {
+          return (
+            <EventFeedItem
+              key={`event-feed-item-${index}`}
+              {...eventFeedItemProps}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
 
