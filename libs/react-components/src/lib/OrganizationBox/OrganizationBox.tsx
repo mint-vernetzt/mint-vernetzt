9ad7e0e71296db8dafe3008d20919a2d7acd0990
Sirko Stenz <sirko.stenz@digitalnoise.de>
2021-10-15 11:49:19 -0500

    linked logo, added max-height

--- libs/react-components/src/lib/OrganizationBox/OrganizationBox.tsx
+++ libs/react-components/src/lib/OrganizationBox/OrganizationBox.tsx
@@ -14,12 +14,14 @@ export function OrganizationBox(props: OrganizationBoxProps) {
   const { name, description, organizationUrl, organizationLogo } = props;
   return (
     <div className="flex flex-col p-4 pb-15 md:pb-5 bg-gray-50 rounded-lg shadow-lg h-full">
-      <img
-        data-testid="logo"
-        className="mb-6"
-        src={organizationLogo.src}
-        alt={organizationLogo.alt}
-      />
+      <a href={organizationUrl} data-testid="organizationUrl">
+        <img
+          data-testid="organizationLogo"
+          className="max-h-16 mb-6"
+          src={organizationLogo.src}
+          alt={organizationLogo.alt}
+        />
+      </a>
       <p data-testid="description" className="text-based text-neutral-700">
         {description}
       </p>
