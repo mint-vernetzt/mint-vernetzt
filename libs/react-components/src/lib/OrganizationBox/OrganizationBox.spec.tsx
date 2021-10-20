ebf71cd18baba29be566a1026880ffb9b159516e
Sirko Stenz <sirko.stenz@digitalnoise.de>
2021-10-20 08:47:37 -0500

    changed test content

--- libs/react-components/src/lib/OrganizationBox/OrganizationBox.spec.tsx
+++ libs/react-components/src/lib/OrganizationBox/OrganizationBox.spec.tsx
@@ -1,18 +1,2 @@
-{
-  /*
-import { render } from "@testing-library/react";
-import OrganizationBox from "./OrganizationBox";
-
-describe("OrganizationBox", () => {
-  it("should render successfully", () => {
-    const { baseElement } = render(<OrganizationBox />);
-    expect(baseElement).toBeTruthy();
-  });
-});
-
-*/
-}
-
-{
-  /* TODO: write reasonable tests */
-}
+// TODO: write reasonable tests
+test("renders successfully", () => {});
