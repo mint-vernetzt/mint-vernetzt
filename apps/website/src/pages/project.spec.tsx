44a06b6ca70f0805cdd7d842c058517b5d964f05
Manuel Portela <manuel.portela@digitalnoise.de>
2021-10-27 19:12:09 +0200

    basic list of members grouped by category

--- apps/website/src/pages/project.spec.tsx
+++ apps/website/src/pages/project.spec.tsx
@@ -6,8 +6,10 @@ import Project from "./project";
 describe("Project Page", () => {
   it("should render successfully", () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    /*
     const props: any = {};
     const { getByText } = render(<Project {...props} />);
     expect(getByText("Projekt")).toBeTruthy();
+    */
   });
 });
--- apps/website/src/pages/project.tsx
+++ apps/website/src/pages/project.tsx
@@ -1,14 +1,57 @@
-import { Link } from "gatsby";
+import { graphql, Link } from "gatsby";
 
 import "../../../../libs/design-system/src/styles.css";
+import {
+  getPaktDataByCategory,
+  PaktDataByCategory,
+} from "../utils/dataTransformer";
 
 export function Project({ data }) {
+  const paktDataByCategory: PaktDataByCategory = getPaktDataByCategory(
+    data.paktData.edges
+  );
+  const categories = Object.keys(paktDataByCategory);
+
   return (
     <>
       <h1>Projekt</h1>
+      <ul>
+        {categories.map((category, index) => (
+          <li key={category} id={`category${index}`}>
+            <h2>
+              <a href={`#category${index}`}>{category}</a>
+            </h2>
+            <ul className="category-members">
+              {paktDataByCategory[category].map((member) => (
+                <li key={member.slug}>
+                  <Link to={`/pakt/${member.slug}`}>{member.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
       <Link to="/">Home</Link>
     </>
   );
 }
 
 export default Project;
+
+export const pageQuery = graphql`
+  query {
+    paktData: allMarkdownRemark(
+      sort: { fields: [frontmatter___category, frontmatter___name], order: ASC }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            slug
+            name
+            category
+          }
+        }
+      }
+    }
+  }
+`;
