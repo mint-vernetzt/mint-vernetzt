94b300fcfee044107e58c377882b72a6ff051751
Manuel Portela <manuel.portela@digitalnoise.de>
2021-10-27 08:27:10 +0200

    initial project page and a link to it on the frontpage

---
+++ apps/website/src/pages/project.tsx
@@ -0,0 +1,14 @@
+import { Link } from "gatsby";
+
+import "../../../../libs/design-system/src/styles.css";
+
+export function Project({ data }) {
+  return (
+    <>
+      <h1>Projekt</h1>
+      <Link to="/">Home</Link>
+    </>
+  );
+}
+
+export default Project;
