68bc9901009a100a031ee49cd9dd2517bc0c08ce
Manuel Portela <manuel.portela@digitalnoise.de>
2021-10-27 18:32:32 +0200

    added testimonial component to index export

--- libs/react-components/src/index.ts
+++ libs/react-components/src/index.ts
@@ -6,3 +6,4 @@ export * from "./lib/EventFeed/EventFeedItem";
 export * from "./lib/Text";
 export * from "./lib/UserCard/UserCard";
 export * from "./lib/UserCard/UserCardContainer";
+export * from "./lib/Testimonial/Testimonial";
