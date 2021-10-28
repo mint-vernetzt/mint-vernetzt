bf48b7adb0f157563d49c03504b43afaf5bbd26d
Sirko Stenz <sirko.stenz@digitalnoise.de>
2021-10-28 10:11:26 -0500

    styling project page

--- apps/website/src/pages/project.tsx
+++ apps/website/src/pages/project.tsx
@@ -1,6 +1,6 @@
+import Layout from "../components/layout";
 import { graphql, Link } from "gatsby";
 
-import "../../../../libs/design-system/src/styles.css";
 import {
   getPaktDataByCategory,
   PaktDataByCategory,
@@ -14,26 +14,36 @@ export function Project({ data }) {
   const categories = Object.keys(paktDataByCategory);
 
   return (
-    <>
-      <h1>Projekt</h1>
-      <ul>
-        {categories.map((category, index) => (
-          <li key={category} id={`category${index}`}>
-            <h2>
-              <a href={`#category${index}`}>{category}</a>
-            </h2>
-            <ul>
-              {paktDataByCategory[category].map((member) => (
-                <li>
-                  <Link to={`/pakt/${member.slug}`}>{member.name}</Link>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
-      <Link to="/">Home</Link>
-    </>
+    <Layout>
+      <section className="container my-16">
+        <h1>Projekt</h1>
+
+        <ul>
+          {categories.map((category, index) => (
+            <li key={category} id={`category${index}`}>
+              <h2>
+                <a href={`#category${index}`}>{category}</a>
+              </h2>
+              <ul>
+                {paktDataByCategory[category].map((member) => (
+                  <li>
+                    <Link to={`/pakt/${member.slug}`}>{member.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+        <p className="my-8">
+          <Link
+            to="/"
+            className="m-1 px-4 py-2 border-2 inline-block rounded border-transparent text-white focus:border-fill focus:border-dotted focus:outline-none filter hover:brightness-75 bg-primary"
+          >
+            Home
+          </Link>
+        </p>
+      </section>
+    </Layout>
   );
 }
 
