bc66e001b8b7fc2cbd3b0cb401dbf98664c08861
Sirko Stenz <sirko.stenz@digitalnoise.de>
2021-10-28 09:45:35 -0500

    added header and footer to layout in gatsby

--- apps/website/src/components/layout.tsx
+++ apps/website/src/components/layout.tsx
@@ -1,19 +1,18 @@
-import React from "react";
-import NavigationBar from "../../../../libs/react-components/src/lib/NavigationBar/NavigationBar";
-import FooterSection from "../../../../libs/react-components/src/lib/FooterSection/FooterSection";
+import Header from "./header";
+import Footer from "./footer";
 import "../../../../libs/design-system/src/styles.css";
 
 function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen font-sans text-gray-900">
       <header>
-        <NavigationBar />
+        <Header />
       </header>
 
       <main className="flex-1 pb-20">{children}</main>
 
       <footer>
-        <FooterSection />
+        <Footer />
       </footer>
     </div>
   );
