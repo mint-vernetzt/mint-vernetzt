9d664e125aae3b981ec1a2d3b0c1fd8dad2cfa00
Peter Holló <hello@songsforthe.dev>
2021-11-04 14:01:10 +0100

    show some details

--- apps/website/src/templates/News.tsx
+++ apps/website/src/templates/News.tsx
@@ -1,16 +1,43 @@
 import { graphql } from "gatsby";
 import React from "react";
+import Img from "gatsby-image";
 
 function News({ data }) {
   const props = data.allWpNewsItem.nodes[0];
   return (
     <div>
+      <p>{props.date}</p>
+      <ul>
+        {props.tags.nodes.map((tag, index) => {
+          return <li key={`tag-${index}`}>{tag.name}</li>;
+        })}
+      </ul>
+      <Img fluid={props.featuredImage.node.localFile.childImageSharp.fluid} />
       <h1>{props.title}</h1>
       <div
         dangerouslySetInnerHTML={{
           __html: props.content,
         }}
       />
+      <h2>Documents</h2>
+      <ul>
+        {props.documents.documentList.map((documentListItem, index) => {
+          const { document } = documentListItem;
+          return (
+            <li key={`document-${index}`}>
+              {document.mimeType}
+              <br />
+              <a
+                href={document.localFile.publicURL}
+                target="_blank"
+                dangerouslySetInnerHTML={{
+                  __html: document.caption,
+                }}
+              ></a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
@@ -21,8 +48,39 @@ export const query = graphql`
   query ($slug: String!) {
     allWpNewsItem(filter: { slug: { eq: $slug } }) {
       nodes {
+        date
         title
         content
+        excerpt
+        slug
+        tags {
+          nodes {
+            name
+          }
+        }
+        documents {
+          documentList {
+            document {
+              title
+              mimeType
+              caption
+              localFile {
+                publicURL
+              }
+            }
+          }
+        }
+        featuredImage {
+          node {
+            localFile {
+              childImageSharp {
+                fluid(maxWidth: 300) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
