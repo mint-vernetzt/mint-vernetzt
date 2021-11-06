a8793b434a10fcf00e789a16617ad3046d51c3a5
Sirko Stenz <sirko.stenz@digitalnoise.de>
2021-11-06 16:38:47 -0500

    added footer changes, changed container margin to padding

--- apps/website/src/pages/events.tsx
+++ apps/website/src/pages/events.tsx
@@ -5,13 +5,13 @@ export function Events({ data }) {
   return (
     <Layout>
       <SEO title="Veranstaltungen" slug="/events" />
-      <section className="hero container my-10">
+      <section className="hero container py-10">
         <div className="hero bg-yellow-300 px-20 py-40 rounded-3xl">
           <h1>Events Teaser 2Spalter</h1>
         </div>
       </section>
 
-      <section className="container my-10">
+      <section className="container py-10">
         <div className="bg-yellow-300 p-20 rounded-3xl">
           <h1>Events Feed Modul</h1>
         </div>
--- apps/website/src/templates/News.tsx
+++ apps/website/src/templates/News.tsx
@@ -17,7 +17,7 @@ function News({ data }) {
         children=""
       />
 
-      <section className="container my-4 md:my-8">
+      <section className="container py-4 md:py-8">
         <Link
           className="inline-block border border-neutral-400 py-3 px-4 mb-6 text-neutral-800 text-semibold uppercase rounded-lg"
           to="/news"
