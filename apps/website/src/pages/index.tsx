e6985e761854c8eb11b4a68c0ed99ca26f451713
Sirko Stenz <sirko.stenz@digitalnoise.de>
2021-11-08 07:32:23 -0500

    feat(website): added get active boxes (#135)
    * added get active boxes on landing page
    
    Closes #134

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -31,20 +31,77 @@ export function Index({ data }) {
         </div>
       </section>
 
-      <section className="container my-10">
-        <div className="bg-yellow-300 p-20 rounded-3xl">
-          <h1>Aktiv werden</h1>
+      <section className="container my-20">
+        <header>
+          <h2>Aktiv werden</h2>
+          <p className="lead">
+            Proin id turpis quis massa mattis consequat. Maecenas semper, et
+            laoreet risus lorem id lorem. Etiam imperdiet. Felis vel auctor
+            consequatProin id turpis quis massa mattis consequat.{" "}
+          </p>
+        </header>
+
+        <div className="grid gap-6 lg:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {[
+            {
+              image: `https://picsum.photos/id/101/420/210`,
+              title: `Aktiv werden`,
+              text: `Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+              sadipscing elitr.`,
+              link: `/project `,
+            },
+            {
+              image: `https://picsum.photos/id/1053/420/210`,
+              title: `Mintvernetzt informiert`,
+              text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+              nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat`,
+              link: `/project `,
+            },
+            {
+              image: `https://picsum.photos/id/24/420/210`,
+              title: `Werde teil der MINTcommunity`,
+              text: `Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+              sadipscing elitr. Lorem ipsum dolor sit amet.`,
+              link: `/topics `,
+            },
+          ].map((teaserbox, index) => (
+            <div
+              key={`teaserbox-${index}`}
+              className="p-2 rounded-lg bg-beige-500"
+            >
+              <a href={`${teaserbox.link}`} className="flex flex-col h-100">
+                <div className="rounded-lg overflow-hidden mb-4">
+                  <img
+                    src={`${teaserbox.image}`}
+                    alt={`${teaserbox.title}`}
+                    className="w-full h-auto"
+                  />
+                </div>
+                <h4 className="text-blue-500 mx-1 mb-2">{teaserbox.title}</h4>
+                <p className="text-sm text-neutral-600 mx-1 mb-4">
+                  {teaserbox.text}
+                </p>
+              </a>
+            </div>
+          ))}
         </div>
       </section>
 
-      <section className="container my-10">
-        <NewsFeed
-          headline="Neuigkeiten"
-          body="Wir blicken in die MINT-Welt und darüber hinaus und informieren Euch regelmäßig über Erkenntnisse, Ereignisse und Innovationen, die für Eure tägliche Arbeit interessant sind."
-          linkToOverview="/news/"
-          newsFeedItemsProps={newsItems}
-        />
-      </section>
+      <div className="container my-10">
+        <div className="flex flex-wrap md:-mx-2 lg:-mx-4">
+          <div className="flex-100 md:flex-2/3 md:px-2 lg:px-4">
+            <NewsFeed
+              headline="Neuigkeiten"
+              body="Wir blicken in die MINT-Welt und darüber hinaus und informieren Euch regelmäßig über Erkenntnisse, Ereignisse und Innovationen, die für Eure tägliche Arbeit interessant sind."
+              linkToOverview="/news/"
+              newsFeedItemsProps={newsItems}
+            />
+          </div>
+          <div className="flex-100 md:flex-1/3 pb-8 md:pb-0 md:px-2 lg:px-4">
+            <div className="bg-yellow-300 p-20 rounded-3xl">Events-Modul</div>
+          </div>
+        </div>
+      </div>
 
       <section className="container my-10">
         <OrganizationBoxContainer
