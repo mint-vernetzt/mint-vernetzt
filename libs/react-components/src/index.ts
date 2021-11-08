a97d65a868f8cf98ddd8abacdea611ef0ce9595e
Peter Holló <hello@songsforthe.dev>
2021-11-08 09:16:10 +0100

    move Image interface to types collection

--- libs/react-components/src/index.ts
+++ libs/react-components/src/index.ts
@@ -8,3 +8,4 @@ export * from "./lib/Text";
 export * from "./lib/UserCard/UserCard";
 export * from "./lib/UserCard/UserCardContainer";
 export * from "./lib/Testimonial/Testimonial";
+export * from "./lib/types.d";
