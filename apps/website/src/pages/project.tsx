63713e43cb7b33a7d6ac8408b7d2541838ba5940
Peter Holló <hello@songsforthe.dev>
2021-11-09 17:33:09 +0100

    replace some "wrong" spaces

--- apps/website/src/pages/project.tsx
+++ apps/website/src/pages/project.tsx
@@ -63,16 +63,16 @@ export function Project({ data }: { data: GatsbyTypes.ProjectPageQuery }) {
             </h1>
 
             <p className="lg:text-3xl text-neutral-600 mb-4 font-bold">
-              Die Service- und Anlaufstelle für MINT-Akteur:innen in Deutschland
+              Die Service- und Anlaufstelle für MINT-Akteur:innen in Deutschland
             </p>
 
             <p className="lg:text-xl text-neutral-600 mb-4">
               Die MINT-Vernetzungsstelle, kurz MINTvernetzt, ist das Dach für
-              die außerschulische MINT-Bildung in Deutschland. MINTvernetzt wird
+              die außerschulische MINT-Bildung in Deutschland. MINTvernetzt wird
               vom Bundesministerium für Bildung und Forschung gefördert und von
-              Mitarbeitenden der Körber-Stiftung, der matrix gGmbH, dem
-              Nationalen MINTForum e.V., dem Stifterverband und der Universität
-              Regensburg als Verbund gemeinsam umgesetzt. 
+              Mitarbeitenden der Körber-Stiftung, der matrix gGmbH, dem
+              Nationalen MINTForum e.V., dem Stifterverband und der Universität
+              Regensburg als Verbund gemeinsam umgesetzt.
             </p>
           </div>
         </div>
@@ -84,22 +84,22 @@ export function Project({ data }: { data: GatsbyTypes.ProjectPageQuery }) {
             {
               icon: `none`,
               title: `Motivation`,
-              text: `MINT-Kompetenzen sind Zukunftskompetenzen. Ohne die Stärkung der MINT-Bildung und Aktivierung neuer MINT-Talente wird Deutschland als Innovationsstandort an Bedeutung verlieren und wichtige Beiträge zur Lösungsfindung globaler Herausforderungen wie dem Klimawandel nicht leisten können. Kindern und Jugendlichen Kompetenzen in Mathematik, Informatik, Naturwissenschaften und Technik zu vermitteln und ihnen den Zugang zu MINT-Arbeitsplätzen zu ermöglichen, sind zentrale Schlüssel für mehr Gleichberechtigung und Teilhabe.`,
+              text: `MINT-Kompetenzen sind Zukunftskompetenzen. Ohne die Stärkung der MINT-Bildung und Aktivierung neuer MINT-Talente wird Deutschland als Innovationsstandort an Bedeutung verlieren und wichtige Beiträge zur Lösungsfindung globaler Herausforderungen wie dem Klimawandel nicht leisten können. Kindern und Jugendlichen Kompetenzen in Mathematik, Informatik, Naturwissenschaften und Technik zu vermitteln und ihnen den Zugang zu MINT-Arbeitsplätzen zu ermöglichen, sind zentrale Schlüssel für mehr Gleichberechtigung und Teilhabe.`,
             },
             {
               icon: `none`,
               title: `Mission`,
-              text: `Wir wollen Kindern und Jugendlichen eine gute MINT-Bildung ermöglichen, die schulisches und außerschulisches Lernen miteinander verzahnt und anwendungsorientiert sowie praxisnah gestaltet ist. Dafür stärken wir das Feld der außerschulischen MINT-Bildung, sorgen für Vernetzung, Sichtbarkeit und Professionalisierung der MINT-Akteur:innen und setzen uns für das Zusammenwirken von Schulen und außerschulischen Partner:innen ein.`,
+              text: `Wir wollen Kindern und Jugendlichen eine gute MINT-Bildung ermöglichen, die schulisches und außerschulisches Lernen miteinander verzahnt und anwendungsorientiert sowie praxisnah gestaltet ist. Dafür stärken wir das Feld der außerschulischen MINT-Bildung, sorgen für Vernetzung, Sichtbarkeit und Professionalisierung der MINT-Akteur:innen und setzen uns für das Zusammenwirken von Schulen und außerschulischen Partner:innen ein.`,
             },
             {
               icon: `none`,
               title: `Ziel`,
-              text: `Wir unterstützen Engagierte der MINT-Bildung durch Vernetzungsräume, Transferangebote und Innovationsimpulse darin, noch bessere Bildungsangebote für Kinder und Jugendliche zu machen und dabei breitere und diversere Zielgruppen anzusprechen. Hierzu zählen insbesondere Mädchen und junge Frauen.`,
+              text: `Wir unterstützen Engagierte der MINT-Bildung durch Vernetzungsräume, Transferangebote und Innovationsimpulse darin, noch bessere Bildungsangebote für Kinder und Jugendliche zu machen und dabei breitere und diversere Zielgruppen anzusprechen. Hierzu zählen insbesondere Mädchen und junge Frauen.`,
             },
             {
               icon: `none`,
               title: `Vision`,
-              text: `Mit MINTvernetzt schaffen wir ein lebendiges, diverses und nachhaltig wachsendes Netzwerk für die MINT-Bildungslandschaft. Wir stellen praxisorientierte Informationen bereit, bieten echten Transfer in die Bildungspraxis und geben den Raum für Diskurse und praktischen Austausch. MINT-Akteur:innen werden sichtbar, begreifen sich als Gemeinschaft und verzahnen ihre Angebote enger mit schulischen Aktivitäten. Sie nehmen neue Themenimpulse auf, gestalten innovative Angebote und erreichen diversere Zielgruppen. Dadurch fördern wir gesellschaftliche Teilhabe und Innovation.`,
+              text: `Mit MINTvernetzt schaffen wir ein lebendiges, diverses und nachhaltig wachsendes Netzwerk für die MINT-Bildungslandschaft. Wir stellen praxisorientierte Informationen bereit, bieten echten Transfer in die Bildungspraxis und geben den Raum für Diskurse und praktischen Austausch. MINT-Akteur:innen werden sichtbar, begreifen sich als Gemeinschaft und verzahnen ihre Angebote enger mit schulischen Aktivitäten. Sie nehmen neue Themenimpulse auf, gestalten innovative Angebote und erreichen diversere Zielgruppen. Dadurch fördern wir gesellschaftliche Teilhabe und Innovation.`,
             },
           ].map((teaser, index) => (
             <div
@@ -118,8 +118,8 @@ export function Project({ data }: { data: GatsbyTypes.ProjectPageQuery }) {
         <header>
           <h2>Für jede Frage die richtige Adresse</h2>
           <p className="lead">
-            Ihr habt eine Frage, sucht Informationen oder möchtet eine Idee mit
-            uns teilen? Wir sind als Ansprechpartner:innen von MINTvernetzt
+            Ihr habt eine Frage, sucht Informationen oder möchtet eine Idee mit
+            uns teilen? Wir sind als Ansprechpartner:innen von MINTvernetzt
             gerne für Euch da.
           </p>
         </header>
@@ -190,7 +190,7 @@ export function Project({ data }: { data: GatsbyTypes.ProjectPageQuery }) {
       <section className="container my-16">
         <UserCardContainer
           headline="Das Team"
-          body="Wir sind MINTvernetzt – ein junges, engagiertes Team mit unterschiedlichen Erfahrungen, Kompetenzen und Hintergründen. Hier findet Ihr alle Teammitglieder mit ihren Aufgaben und Kontaktdaten. Wir freuen uns auf den Austausch mit Euch."
+          body="Wir sind MINTvernetzt – ein junges, engagiertes Team mit unterschiedlichen Erfahrungen, Kompetenzen und Hintergründen. Hier findet Ihr alle Teammitglieder mit ihren Aufgaben und Kontaktdaten. Wir freuen uns auf den Austausch mit Euch."
           userCardsProps={userCardsProps}
         />
       </section>
