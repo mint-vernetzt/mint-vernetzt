9a44a247e85c546126cc7d6b26c3fb90b9551586
Peter Holló <hello@songsforthe.dev>
2021-11-17 18:39:23 +0100

    fix(website): banner classes not loaded

--- apps/website/src/components/layout.tsx
+++ apps/website/src/components/layout.tsx
@@ -1,10 +1,28 @@
 import Header from "./header";
 import Footer from "./footer";
 import "../../../../libs/design-system/src/styles.css";
+import { Link } from "gatsby";
 
 function Layout({ children, ...otherProps }) {
   return (
     <div className="flex flex-col min-h-screen font-sans text-gray-600">
+      {otherProps.path === "/" ? (
+        <div className="bg-lilac-300">
+          <div className="container">
+            <p className="text-center px-4 py-2 text-white font-bold">
+              Vom 22. bis. 26.11. findet die MINT-Aktionswoche statt. Schaut
+              Euch die{" "}
+              <Link
+                to="/event/auftaktveranstaltung"
+                className="hover:underline"
+              >
+                Auftaktveranstaltung im Livestream
+              </Link>{" "}
+              an.
+            </p>
+          </div>
+        </div>
+      ) : null}
       <header>
         <Header {...otherProps} />
       </header>
