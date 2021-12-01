d2a2b93f59dc9744cb2c6c6012c38887fbd407be
Peter Holló <hello@songsforthe.dev>
2021-12-01 19:46:28 +0100

    build plugin

--- apps/website-backend/web/app/plugins/mint-vernetzt-sidebar/build/index.asset.php
+++ apps/website-backend/web/app/plugins/mint-vernetzt-sidebar/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-media-utils', 'wp-plugins'), 'version' => 'ef4133ce9c3e28df4f02e02367072c0c');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-media-utils', 'wp-plugins'), 'version' => 'ce5c3ad5714a2a5c683160b39b200ce3');
--- apps/website-backend/web/app/plugins/mint-vernetzt-sidebar/build/index.js
+++ apps/website-backend/web/app/plugins/mint-vernetzt-sidebar/build/index.js
@@ -176,41 +176,41 @@
       /*#__PURE__*/ __webpack_require__.n(
         _wordpress_element__WEBPACK_IMPORTED_MODULE_0__
       );
-    /* harmony import */ var _wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__ =
-      __webpack_require__(/*! @wordpress/plugins */ "@wordpress/plugins");
-    /* harmony import */ var _wordpress_plugins__WEBPACK_IMPORTED_MODULE_1___default =
-      /*#__PURE__*/ __webpack_require__.n(
-        _wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__
-      );
-    /* harmony import */ var _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2__ =
-      __webpack_require__(/*! @wordpress/edit-post */ "@wordpress/edit-post");
-    /* harmony import */ var _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2___default =
-      /*#__PURE__*/ __webpack_require__.n(
-        _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2__
-      );
-    /* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_3__ =
-      __webpack_require__(/*! @wordpress/data */ "@wordpress/data");
-    /* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_3___default =
-      /*#__PURE__*/ __webpack_require__.n(
-        _wordpress_data__WEBPACK_IMPORTED_MODULE_3__
-      );
-    /* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_4__ =
+    /* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_1__ =
       __webpack_require__(/*! @wordpress/components */ "@wordpress/components");
-    /* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_4___default =
+    /* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_1___default =
       /*#__PURE__*/ __webpack_require__.n(
-        _wordpress_components__WEBPACK_IMPORTED_MODULE_4__
+        _wordpress_components__WEBPACK_IMPORTED_MODULE_1__
       );
-    /* harmony import */ var _wordpress_media_utils__WEBPACK_IMPORTED_MODULE_5__ =
+    /* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_2__ =
+      __webpack_require__(/*! @wordpress/data */ "@wordpress/data");
+    /* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_2___default =
+      /*#__PURE__*/ __webpack_require__.n(
+        _wordpress_data__WEBPACK_IMPORTED_MODULE_2__
+      );
+    /* harmony import */ var _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_3__ =
+      __webpack_require__(/*! @wordpress/edit-post */ "@wordpress/edit-post");
+    /* harmony import */ var _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_3___default =
+      /*#__PURE__*/ __webpack_require__.n(
+        _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_3__
+      );
+    /* harmony import */ var _wordpress_media_utils__WEBPACK_IMPORTED_MODULE_4__ =
       __webpack_require__(
         /*! @wordpress/media-utils */ "@wordpress/media-utils"
       );
-    /* harmony import */ var _wordpress_media_utils__WEBPACK_IMPORTED_MODULE_5___default =
+    /* harmony import */ var _wordpress_media_utils__WEBPACK_IMPORTED_MODULE_4___default =
       /*#__PURE__*/ __webpack_require__.n(
-        _wordpress_media_utils__WEBPACK_IMPORTED_MODULE_5__
+        _wordpress_media_utils__WEBPACK_IMPORTED_MODULE_4__
+      );
+    /* harmony import */ var _wordpress_plugins__WEBPACK_IMPORTED_MODULE_5__ =
+      __webpack_require__(/*! @wordpress/plugins */ "@wordpress/plugins");
+    /* harmony import */ var _wordpress_plugins__WEBPACK_IMPORTED_MODULE_5___default =
+      /*#__PURE__*/ __webpack_require__.n(
+        _wordpress_plugins__WEBPACK_IMPORTED_MODULE_5__
       );
 
     function Attachments() {
-      const files = (0, _wordpress_data__WEBPACK_IMPORTED_MODULE_3__.useSelect)(
+      const files = (0, _wordpress_data__WEBPACK_IMPORTED_MODULE_2__.useSelect)(
         (select) => {
           const meta = select("core/editor").getEditedPostAttribute("meta");
           return meta["attachments"];
@@ -218,15 +218,24 @@
         []
       );
       const { editPost } = (0,
-      _wordpress_data__WEBPACK_IMPORTED_MODULE_3__.useDispatch)("core/editor");
+      _wordpress_data__WEBPACK_IMPORTED_MODULE_2__.useDispatch)("core/editor");
+
+      const handleRemove = (id) => {
+        const filteredFiles = files.filter((file) => file.id !== id);
+        editPost({
+          meta: {
+            attachments: filteredFiles,
+          },
+        });
+      };
+
       return (0, _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
-        _wordpress_media_utils__WEBPACK_IMPORTED_MODULE_5__.MediaUpload,
+        _wordpress_media_utils__WEBPACK_IMPORTED_MODULE_4__.MediaUpload,
         {
           label: "Upload",
           defaultValue: files,
           multiple: true,
           onSelect: (mediaItems) => {
-            console.log("media items", mediaItems);
             const attachments = mediaItems.map((mediaItem) => {
               const {
                 id,
@@ -246,10 +255,20 @@
                 url,
                 subtype,
               };
-            });
+            }); // merge current an added attachment files
+            // remove duplicates
+
+            const allFiles = files
+              .concat(attachments)
+              .filter((file, index, array) => {
+                const indexInArray = array.findIndex(
+                  (item) => item.id === file.id
+                );
+                return indexInArray === index;
+              });
             return editPost({
               meta: {
-                attachments,
+                attachments: allFiles,
               },
             }); // Promise
           },
@@ -266,24 +285,59 @@
                       "div",
                       {
                         key: `attachment-${index}`,
+                        style: {
+                          padding: "1rem",
+                        },
                       },
                       (0,
                       _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
-                        "img",
+                        "div",
                         {
-                          src: file.icon,
-                          alt: "",
-                        }
-                      ),
-                      (0,
-                      _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
-                        "p",
-                        null,
+                          style: {
+                            display: "flex",
+                            gap: "0.5rem",
+                            direction: "row",
+                            alignItems: "baseline",
+                          },
+                        },
                         (0,
                         _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
-                          "strong",
-                          null,
-                          file.title
+                          "div",
+                          {
+                            style: {
+                              width: "1rem",
+                              height: "1rem",
+                              flexShrink: "0",
+                            },
+                          },
+                          (0,
+                          _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
+                            "img",
+                            {
+                              src: file.icon,
+                              alt: "",
+                              style: {
+                                maxWidth: "100%",
+                                height: "auto",
+                                display: "block",
+                              },
+                            }
+                          )
+                        ),
+                        (0,
+                        _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
+                          "p",
+                          {
+                            style: {
+                              flex: "auto",
+                            },
+                          },
+                          (0,
+                          _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
+                            "strong",
+                            null,
+                            file.title
+                          )
                         )
                       ),
                       (0,
@@ -310,27 +364,59 @@
                       ),
                       (0,
                       _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
-                        "p",
-                        null,
+                        "div",
+                        {
+                          style: {
+                            display: "flex",
+                            direction: "row",
+                            justifyContent: "space-between",
+                            alignItems: "baseline",
+                          },
+                        },
                         (0,
                         _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
-                          "strong",
+                          "p",
                           null,
-                          "File size:"
+                          (0,
+                          _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
+                            "strong",
+                            null,
+                            "File size:"
+                          ),
+                          " ",
+                          file.fileSizeHumanReadable
                         ),
-                        " ",
-                        file.fileSizeHumanReadable
+                        (0,
+                        _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
+                          _wordpress_components__WEBPACK_IMPORTED_MODULE_1__.Button,
+                          {
+                            isSmall: true,
+                            isTertiary: true,
+                            onClick: () => handleRemove(file.id),
+                          },
+                          "Remove"
+                        )
                       )
                     );
                   })
                 : null,
               (0,
               _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
-                _wordpress_components__WEBPACK_IMPORTED_MODULE_4__.Button,
+                "div",
                 {
-                  onClick: open,
+                  style: {
+                    padding: "1rem",
+                  },
                 },
-                "Add Attachments"
+                (0,
+                _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
+                  _wordpress_components__WEBPACK_IMPORTED_MODULE_1__.Button,
+                  {
+                    isPrimary: true,
+                    onClick: open,
+                  },
+                  "Add Attachments"
+                )
               )
             );
           },
@@ -338,13 +424,13 @@
       );
     }
 
-    (0, _wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__.registerPlugin)(
+    (0, _wordpress_plugins__WEBPACK_IMPORTED_MODULE_5__.registerPlugin)(
       "mint-vernetzt-sidebar",
       {
         render: (props) => {
           return (0,
           _wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(
-            _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2__.PluginSidebar,
+            _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_3__.PluginSidebar,
             {
               name: "mint-vernetzt-sidebar",
               icon: "admin-post",
