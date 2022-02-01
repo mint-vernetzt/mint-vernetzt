2c2c9e7365dcaa44258a4a54da998afdfe17062a
Peter Holló <hello@songsforthe.dev>
2022-02-01 14:58:52 +0100

    use component for color list

--- libs/design-system/src/stories/Colors.stories.tsx
+++ libs/design-system/src/stories/Colors.stories.tsx
@@ -1,199 +1,76 @@
 import { Meta, Story } from "@storybook/react";
 import { colors } from "../theme";
 
+type ColorItemProps = {
+  color: string;
+  name?: string;
+  withBorder?: boolean;
+};
+
+function ColorItem(props: ColorItemProps) {
+  return (
+    <div className="flex items-center">
+      <div
+        className={`flex-none bg-${
+          props.color
+        } w-1 h-1 p-4 mr-1 rounded-md border ${
+          props.withBorder ? "border-neutral-500" : "border-transparent"
+        }`}
+      ></div>
+      <div className="flex-1">
+        {props.withBorder}
+        <span className="block font-bold text-sm">
+          {props.name || props.color}
+        </span>
+        <span className="text-sm">{colors[props.color]}</span>
+      </div>
+    </div>
+  );
+}
+
 export const Colors: Story = () => {
   return (
     <div className="min-w-max max-w-4xl">
       <div className="grid grid-cols-5 gap-2 w-full">
-        <div className="flex items-center">
-          <div className="flex-none bg-primary w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">primary/primary-500</p>
-            <p className="text-sm">{colors.primary}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-primary-400 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">primary-400</p>
-            <p className="text-sm">{colors["primary-400"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-primary-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">primary-300</p>
-            <p className="text-sm">{colors["primary-300"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-primary-200 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">primary-200</p>
-            <p className="text-sm">{colors["primary-200"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-primary-100 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">primary-100</p>
-            <p className="text-sm">{colors["primary-100"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-secondary w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">secondary/secondary-500</p>
-            <p className="text-sm">{colors.secondary}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-secondary-400 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">secondary-400</p>
-            <p className="text-sm">{colors["secondary-400"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-secondary-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">secondary-300</p>
-            <p className="text-sm">{colors["secondary-300"]}</p>
-          </div>
-        </div>
+        <ColorItem color="primary" name="primary/primary-500" />
+        <ColorItem color="primary-400" />
+        <ColorItem color="primary-300" />
+        <ColorItem color="primary-200" />
+        <ColorItem color="primary-100" />
+        <ColorItem color="secondary" name="secondary/secondary-500" />
+        <ColorItem color="secondary-400" />
+        <ColorItem color="secondary-300" />
         <div></div>
         <div></div>
-        <div className="flex items-center">
-          <div className="flex-none bg-tertiary w-1 h-1 p-4 mr-1 rounded-md border border-transparent border-neutral-500"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">tertiary/tertiary-500</p>
-            <p className="text-sm">{colors.tertiary}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-tertiary-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent border-neutral-500"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">tertiary-300</p>
-            <p className="text-sm">{colors["tertiary-300"]}</p>
-          </div>
-        </div>
+        <ColorItem color="tertiary" name="tertiary/tertiary-500" />
+        <ColorItem color="tertiary-300" withBorder />
         <div></div>
         <div></div>
         <div></div>
-        <div className="flex items-center">
-          <div className="flex-none bg-success w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">success/success-500</p>
-            <p className="text-sm">{colors.success}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-success-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">success-300</p>
-            <p className="text-sm">{colors["success-300"]}</p>
-          </div>
-        </div>
+        <ColorItem color="success" name="success/success-500" />
+        <ColorItem color="success-300" name="success-300" />
         <div></div>
         <div></div>
         <div></div>
-        <div className="flex items-center">
-          <div className="flex-none bg-warning w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">warning/warning-500</p>
-            <p className="text-sm">{colors.warning}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-warning-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">warning-300</p>
-            <p className="text-sm">{colors["warning-300"]}</p>
-          </div>
-        </div>
+        <ColorItem color="warning" name="warning/warning-500" />
+        <ColorItem color="warning-300" name="warning-300" />
         <div></div>
         <div></div>
         <div></div>
-        <div className="flex items-center">
-          <div className="flex-none bg-danger w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">danger/danger-500</p>
-            <p className="text-sm">{colors.danger}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-danger-300 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">danger-300</p>
-            <p className="text-sm">{colors["danger-300"]}</p>
-          </div>
-        </div>
+        <ColorItem color="danger" name="danger/danger-500" />
+        <ColorItem color="danger-300" name="danger-300" />
         <div></div>
         <div></div>
         <div></div>
-        <div className="flex items-center">
-          <div className="flex-none bg-neutral-900 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">neutral-900</p>
-            <p className="text-sm">{colors["neutral-900"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-neutral-800 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">neutral-800</p>
-            <p className="text-sm">{colors["neutral-800"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-neutral-700 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">neutral-700</p>
-            <p className="text-sm">{colors["neutral-700"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-neutral-600 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">neutral-600</p>
-            <p className="text-sm">{colors["neutral-600"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-neutral-500 w-1 h-1 p-4 mr-1 rounded-md border border-transparent"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">neutral-500</p>
-            <p className="text-sm">{colors["neutral-500"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-neutral-400 w-1 h-1 p-4 mr-1 rounded-md border border-neutral-500"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">neutral-400</p>
-            <p className="text-sm">{colors["neutral-400"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-neutral-300 w-1 h-1 p-4 mr-1 rounded-md border border-neutral-500"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">neutral-300</p>
-            <p className="text-sm">{colors["neutral-300"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-neutral-200 w-1 h-1 p-4 mr-1 rounded-md border border-neutral-500"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">neutral-200</p>
-            <p className="text-sm">{colors["neutral-200"]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex-none bg-neutral-100 w-1 h-1 p-4 mr-1 rounded-md border border-neutral-500"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">neutral-100</p>
-            <p className="text-sm">{colors["neutral-100"]}</p>
-          </div>
-        </div>
+        <ColorItem color="neutral-900" />
+        <ColorItem color="neutral-800" />
+        <ColorItem color="neutral-700" />
+        <ColorItem color="neutral-600" />
+        <ColorItem color="neutral-500" />
+        <ColorItem color="neutral-400" withBorder />
+        <ColorItem color="neutral-300" withBorder />
+        <ColorItem color="neutral-200" withBorder />
+        <ColorItem color="neutral-100" withBorder />
       </div>
     </div>
   );
