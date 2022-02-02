edc805beefdec29c8381983b4d2284f69a223922
Peter Holló <hello@songsforthe.dev>
2022-02-02 20:56:10 +0100

    refactor component

--- libs/react-components/src/lib/Heading/Heading.spec.tsx
+++ libs/react-components/src/lib/Heading/Heading.spec.tsx
@@ -1,29 +1,29 @@
 import { render, screen } from "@testing-library/react";
 
-import Heading from "./Heading";
+import Heading, { H1, H2, H3, H4, H5, H6 } from "./Heading";
 
 test("render heading of different levels", () => {
-  render(<Heading.H1>Hello Heading</Heading.H1>);
+  render(<Heading as="h1">Hello Heading</Heading>);
   const heading1DOM = screen.getByRole("heading", { level: 1 });
   expect(heading1DOM).toBeTruthy();
 
-  render(<Heading.H2>Hello Heading</Heading.H2>);
+  render(<Heading as="h2">Hello Heading</Heading>);
   const heading2DOM = screen.getByRole("heading", { level: 2 });
   expect(heading2DOM).toBeTruthy();
 
-  render(<Heading.H3>Hello Heading</Heading.H3>);
+  render(<Heading as="h3">Hello Heading</Heading>);
   const heading3DOM = screen.getByRole("heading", { level: 3 });
   expect(heading3DOM).toBeTruthy();
 
-  render(<Heading.H4>Hello Heading</Heading.H4>);
+  render(<Heading as="h4">Hello Heading</Heading>);
   const heading4DOM = screen.getByRole("heading", { level: 4 });
   expect(heading4DOM).toBeTruthy();
 
-  render(<Heading.H5>Hello Heading</Heading.H5>);
+  render(<Heading as="h5">Hello Heading</Heading>);
   const heading5DOM = screen.getByRole("heading", { level: 5 });
   expect(heading5DOM).toBeTruthy();
 
-  render(<Heading.H6>Hello Heading</Heading.H6>);
+  render(<Heading as="h6">Hello Heading</Heading>);
   const heading6DOM = screen.getByRole("heading", { level: 6 });
   expect(heading6DOM).toBeTruthy();
 });
@@ -31,28 +31,28 @@ test("render heading of different levels", () => {
 test("render heading content", () => {
   const content = "Heading Content";
 
-  render(<Heading.H1>{content}</Heading.H1>);
+  render(<H1>{content}</H1>);
   const headingDOM = screen.getByText(content);
   expect(headingDOM).toBeTruthy();
 });
 
 test("render heading with different style", () => {
-  render(<Heading.H1 as="h2">Hello Heading</Heading.H1>);
+  render(<H1>Hello Heading</H1>);
   const heading1DOM = screen.getByRole("heading", { level: 2 });
   expect(heading1DOM).toBeTruthy();
-  render(<Heading.H2 as="h3">Hello Heading</Heading.H2>);
+  render(<H2>Hello Heading</H2>);
   const heading2DOM = screen.getByRole("heading", { level: 3 });
   expect(heading2DOM).toBeTruthy();
-  render(<Heading.H3 as="h4">Hello Heading</Heading.H3>);
+  render(<H3>Hello Heading</H3>);
   const heading3DOM = screen.getByRole("heading", { level: 4 });
   expect(heading3DOM).toBeTruthy();
-  render(<Heading.H4 as="h5">Hello Heading</Heading.H4>);
+  render(<H4>Hello Heading</H4>);
   const heading4DOM = screen.getByRole("heading", { level: 5 });
   expect(heading4DOM).toBeTruthy();
-  render(<Heading.H5 as="h6">Hello Heading</Heading.H5>);
+  render(<H5>Hello Heading</H5>);
   const heading5DOM = screen.getByRole("heading", { level: 6 });
   expect(heading5DOM).toBeTruthy();
-  render(<Heading.H6 as="h1">Hello Heading</Heading.H6>);
+  render(<H6>Hello Heading</H6>);
   const heading6DOM = screen.getByRole("heading", { level: 1 });
   expect(heading6DOM).toBeTruthy();
 });
--- libs/react-components/src/lib/Heading/Heading.tsx
+++ libs/react-components/src/lib/Heading/Heading.tsx
@@ -1,51 +1,39 @@
 import React from "react";
 
-export interface HeadingProps {
-  children: string | React.ReactNode;
-  as?: string;
+export type HeadingTypes = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+export interface HeadingProps extends React.HTMLProps<HTMLHeadingElement> {
+  as: HeadingTypes;
 }
 
-function getElement(props: {
-  children: string | React.ReactNode;
-  as: string;
-  [index: string]: any;
-}) {
+function Heading(props: HeadingProps) {
   const { as, ...otherProps } = props;
   const element = React.createElement(as, otherProps);
   return element;
 }
 
-export function H1(props: HeadingProps) {
-  const { as = "h1", ...otherProps } = props;
-  return getElement({ as, ...otherProps });
+export function H1(props: React.HTMLProps<HTMLHeadingElement>) {
+  return <Heading {...props} as="h1" />;
 }
 
-export function H2(props: HeadingProps) {
-  const { as = "h2", ...otherProps } = props;
-  return getElement({ as, ...otherProps });
-}
-export function H3(props: HeadingProps) {
-  const { as = "h3", ...otherProps } = props;
-  return getElement({ as, ...otherProps });
-}
-export function H4(props: HeadingProps) {
-  const { as = "h4", ...otherProps } = props;
-  return getElement({ as, ...otherProps });
-}
-export function H5(props: HeadingProps) {
-  const { as = "h5", ...otherProps } = props;
-  return getElement({ as, ...otherProps });
-}
-export function H6(props: HeadingProps) {
-  const { as = "h6", ...otherProps } = props;
-  return getElement({ as, ...otherProps });
+export function H2(props: React.HTMLProps<HTMLHeadingElement>) {
+  return <Heading {...props} as="h2" />;
 }
 
-export default {
-  H1,
-  H2,
-  H3,
-  H4,
-  H5,
-  H6,
-};
+export function H3(props: React.HTMLProps<HTMLHeadingElement>) {
+  return <Heading {...props} as="h3" />;
+}
+
+export function H4(props: React.HTMLProps<HTMLHeadingElement>) {
+  return <Heading {...props} as="h4" />;
+}
+
+export function H5(props: React.HTMLProps<HTMLHeadingElement>) {
+  return <Heading {...props} as="h5" />;
+}
+
+export function H6(props: React.HTMLProps<HTMLHeadingElement>) {
+  return <Heading {...props} as="h6" />;
+}
+
+export default Heading;
