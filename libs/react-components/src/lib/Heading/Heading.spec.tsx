7d288fa0fe079b935549232ea368503e1f266ddb
Peter Holló <hello@songsforthe.dev>
2022-02-02 14:56:56 +0100

    add all levels of headings

--- libs/react-components/src/lib/Heading/Heading.spec.tsx
+++ libs/react-components/src/lib/Heading/Heading.spec.tsx
@@ -1,10 +1,37 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Heading from "./Heading";
 
-describe("Heading", () => {
-  it("should render successfully", () => {
-    const { baseElement } = render(<Heading />);
-    expect(baseElement).toBeTruthy();
-  });
+test("render heading of different levels", () => {
+  render(<Heading.H1>Hello Heading</Heading.H1>);
+  const heading1DOM = screen.getByRole("heading", { level: 1 });
+  expect(heading1DOM).toBeTruthy();
+
+  render(<Heading.H2>Hello Heading</Heading.H2>);
+  const heading2DOM = screen.getByRole("heading", { level: 2 });
+  expect(heading2DOM).toBeTruthy();
+
+  render(<Heading.H3>Hello Heading</Heading.H3>);
+  const heading3DOM = screen.getByRole("heading", { level: 3 });
+  expect(heading3DOM).toBeTruthy();
+
+  render(<Heading.H4>Hello Heading</Heading.H4>);
+  const heading4DOM = screen.getByRole("heading", { level: 4 });
+  expect(heading4DOM).toBeTruthy();
+
+  render(<Heading.H5>Hello Heading</Heading.H5>);
+  const heading5DOM = screen.getByRole("heading", { level: 5 });
+  expect(heading5DOM).toBeTruthy();
+
+  render(<Heading.H6>Hello Heading</Heading.H6>);
+  const heading6DOM = screen.getByRole("heading", { level: 6 });
+  expect(heading6DOM).toBeTruthy();
+});
+
+test("render heading content", () => {
+  const content = "Heading Content";
+
+  render(<Heading.H1>{content}</Heading.H1>);
+  const headingDOM = screen.getByText(content);
+  expect(headingDOM).toBeTruthy();
 });
--- libs/react-components/src/lib/Heading/Heading.tsx
+++ libs/react-components/src/lib/Heading/Heading.tsx
@@ -1,12 +1,43 @@
-/* eslint-disable-next-line */
-export interface HeadingProps {}
+import React from "react";
+
+export interface HeadingProps {
+  children: string | React.ReactNode;
+}
 
 export function Heading(props: HeadingProps) {
+  const { children } = props;
+
   return (
     <div>
-      <h1>Welcome to Heading!</h1>
+      <h2>{children}</h2>
     </div>
   );
 }
 
-export default Heading;
+export function H1(props: HeadingProps) {
+  return <h1>{props.children}</h1>;
+}
+export function H2(props: HeadingProps) {
+  return <h2>{props.children}</h2>;
+}
+export function H3(props: HeadingProps) {
+  return <h3>{props.children}</h3>;
+}
+export function H4(props: HeadingProps) {
+  return <h4>{props.children}</h4>;
+}
+export function H5(props: HeadingProps) {
+  return <h5>{props.children}</h5>;
+}
+export function H6(props: HeadingProps) {
+  return <h6>{props.children}</h6>;
+}
+
+export default {
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+};
