25f52e517425bea550cd6258ac9de7a2b30ac526
Peter Holló <hello@songsforthe.dev>
2022-02-03 13:12:05 +0100

    add heading style type

--- libs/react-components/src/lib/Heading/Heading.tsx
+++ libs/react-components/src/lib/Heading/Heading.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 export type HeadingTypes = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+export type HeadingStyles = "h0" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 export interface HeadingProps extends React.HTMLProps<HTMLHeadingElement> {
   as: HeadingTypes;
-  like?: HeadingTypes;
+  like?: HeadingStyles;
 }
 
 function Heading(props: HeadingProps) {
@@ -24,37 +25,37 @@ function Heading(props: HeadingProps) {
 }
 
 export function H1(
-  props: { like?: HeadingTypes } & React.HTMLProps<HTMLHeadingElement>
+  props: { like?: HeadingStyles } & React.HTMLProps<HTMLHeadingElement>
 ) {
   return <Heading {...props} as="h1" />;
 }
 
 export function H2(
-  props: { like?: HeadingTypes } & React.HTMLProps<HTMLHeadingElement>
+  props: { like?: HeadingStyles } & React.HTMLProps<HTMLHeadingElement>
 ) {
   return <Heading {...props} as="h2" />;
 }
 
 export function H3(
-  props: { like?: HeadingTypes } & React.HTMLProps<HTMLHeadingElement>
+  props: { like?: HeadingStyles } & React.HTMLProps<HTMLHeadingElement>
 ) {
   return <Heading {...props} as="h3" />;
 }
 
 export function H4(
-  props: { like?: HeadingTypes } & React.HTMLProps<HTMLHeadingElement>
+  props: { like?: HeadingStyles } & React.HTMLProps<HTMLHeadingElement>
 ) {
   return <Heading {...props} as="h4" />;
 }
 
 export function H5(
-  props: { like?: HeadingTypes } & React.HTMLProps<HTMLHeadingElement>
+  props: { like?: HeadingStyles } & React.HTMLProps<HTMLHeadingElement>
 ) {
   return <Heading {...props} as="h5" />;
 }
 
 export function H6(
-  props: { like?: HeadingTypes } & React.HTMLProps<HTMLHeadingElement>
+  props: { like?: HeadingStyles } & React.HTMLProps<HTMLHeadingElement>
 ) {
   return <Heading {...props} as="h6" />;
 }
