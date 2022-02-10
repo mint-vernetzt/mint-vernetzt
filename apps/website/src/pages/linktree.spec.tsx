fc5f4a17d27f9586a50c5b1f1ec41bc6805d02e3
Peter Holló <hello@songsforthe.dev>
2022-02-10 14:29:28 +0100

    feat(website): link tree (#304)
    * added dummy link tree page
    * added wp plugin Post Types Order
    * query data from backend
    
    Co-authored-by: Sirko Stenz <sirko.stenz@digitalnoise.de>
    Co-authored-by: Manuel Portela <manuel.portela@digitalnoise.de>

---
+++ apps/website/src/pages/linktree.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+
+import Linktree from "./linktree";
+
+describe("Linktree", () => {
+  it("should render successfully", () => {
+    /*
+    const { baseElement } = render(<Imprint />);
+    expect(baseElement).toBeTruthy();
+    */
+  });
+});
