3376319f8374cfa41010e9304969c5821be34aee
Peter Holló <hello@songsforthe.dev>
2022-02-14 19:17:40 +0100

    add basic tests

--- libs/react-components/src/lib/Link/Link.spec.tsx
+++ libs/react-components/src/lib/Link/Link.spec.tsx
@@ -1,10 +1,60 @@
-import { render } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 
 import Link from "./Link";
 
-describe("Link", () => {
-  it("should render successfully", () => {
-    const { baseElement } = render(<Link />);
-    expect(baseElement).toBeTruthy();
-  });
+test("render link with text", () => {
+  const route = "/home";
+  const text = "Home";
+
+  render(<Link to={route}>{text}</Link>);
+
+  const linkDom = screen.getByRole("link");
+  expect(linkDom.getAttribute("href")).toBe(route);
+  expect(linkDom.innerText).toBe(text);
+});
+
+test("render external link", () => {
+  render(
+    <Link to="https://external.site" isExternal>
+      External
+    </Link>
+  );
+
+  const linkDom = screen.getByRole("link");
+  expect(linkDom.getAttribute("rel")).toBe("noopener noreferrer");
+  expect(linkDom.getAttribute("target")).toBe("_blank");
+});
+
+test("wrap children inside link", () => {
+  const text = "Headline";
+
+  render(
+    <Link to="/whatever">
+      <h1>{text}</h1>
+    </Link>
+  );
+
+  const linkDom = screen.getByRole("link");
+  const headlineDom = within(linkDom).getByRole("heading");
+  expect(headlineDom.innerText).toBe(text);
+});
+
+test("use passed component as link", () => {
+  const testId = "component";
+  const route = "/home";
+  const text = "Home";
+
+  const Component = (props: any) => {
+    return <span data-testId={testId} {...props} />;
+  };
+
+  render(
+    <Link as={Component} to={route}>
+      {text}
+    </Link>
+  );
+
+  const linkDom = screen.getByTestId(testId);
+  expect(linkDom.getAttribute("to")).toBe(route);
+  expect(linkDom.innerText).toBe(text);
 });
