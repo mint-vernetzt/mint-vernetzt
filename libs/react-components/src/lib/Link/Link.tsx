27ab2f844ed2f18708ca7947292b02b97ef96d83
Peter Holló <hello@songsforthe.dev>
2022-02-14 19:52:25 +0100

    implement component

--- libs/react-components/src/lib/Link/Link.tsx
+++ libs/react-components/src/lib/Link/Link.tsx
@@ -1,12 +1,35 @@
-/* eslint-disable-next-line */
-export interface LinkProps {}
-
-export function Link(props: LinkProps) {
-  return (
-    <div>
-      <h1>Welcome to Link!</h1>
-    </div>
-  );
+import React from "react";
+export interface LinkProps {
+  to: string;
+  children: string | React.ReactNode;
+  as?: string | React.ElementType;
+  isExternal?: boolean;
 }
+
+export const Link = React.forwardRef((props: LinkProps, ref) => {
+  const { to, isExternal = false, as = "a", ...otherProps } = props;
+
+  let rel;
+  let target;
+  if (isExternal) {
+    rel = "noopener noreferrer";
+    target = "_blank";
+  }
+
+  let href;
+  if (as === "a") {
+    href = to;
+  }
+
+  const element = React.createElement(as, {
+    href,
+    to,
+    rel,
+    target,
+    ref,
+    ...otherProps,
+  });
+  return element;
+});
 
 export default Link;
