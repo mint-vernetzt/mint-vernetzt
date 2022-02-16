0e6c854e43961883b328544a6f90017175094c9f
Peter Holló <hello@songsforthe.dev>
2022-02-16 16:18:20 +0100

    add variants story

--- libs/react-components/src/lib/Button/index.stories.tsx
+++ libs/react-components/src/lib/Button/index.stories.tsx
@@ -2,20 +2,20 @@ import { Story, Meta } from "@storybook/react";
 import { Button, ButtonProps } from "./Button";
 
 export default {
-  title: "Button",
+  title: "Components/Button",
 } as Meta;
 
-export const Default: Story<ButtonProps> = (args) => {
+export const Playground: Story<ButtonProps> = (args) => {
   return <Button {...args} />;
 };
-Default.args = {
+Playground.args = {
   disabled: false,
   submit: false,
   small: false,
   children: "Button",
   variant: "primary",
 };
-Default.argTypes = {
+Playground.argTypes = {
   children: {
     type: "string",
   },
@@ -25,4 +25,65 @@ Default.argTypes = {
   },
   onClick: { action: "button clicked" },
 };
-Default.storyName = "Default";
+Playground.storyName = "Playground";
+
+export const Variants: Story = () => {
+  return (
+    <>
+      <span className="block m-2">
+        <Button>Primary</Button>
+      </span>
+      <span className="block m-2">
+        <Button disabled>Primary disabled</Button>
+      </span>
+      <span className="block m-2">
+        <Button small>Primary small</Button>
+      </span>
+      <span className="block m-2">
+        <Button small disabled>
+          Primary small disabled
+        </Button>
+      </span>
+      <span className="block m-2">
+        <Button variant="neutral">Neutral</Button>
+      </span>
+      <span className="block m-2">
+        <Button variant="neutral" disabled>
+          Neutral disabled
+        </Button>
+      </span>
+      <span className="block m-2">
+        <Button variant="neutral" small>
+          Neutral small
+        </Button>
+      </span>
+      <span className="block m-2">
+        <Button variant="neutral" small disabled>
+          Neutral small disabled
+        </Button>
+      </span>
+      <span className="block m-2">
+        <Button variant="ghost">Ghost</Button>
+      </span>
+      <span className="block m-2">
+        <Button variant="ghost" disabled>
+          Ghost disabled
+        </Button>
+      </span>
+      <span className="block m-2">
+        <Button variant="ghost" small>
+          Ghost small
+        </Button>
+      </span>
+      <span className="block m-2">
+        <Button variant="ghost" small disabled>
+          Ghost small disabled
+        </Button>
+      </span>
+    </>
+  );
+};
+Variants.parameters = {
+  controls: { disable: true },
+};
+Variants.storyName = "Variants";
